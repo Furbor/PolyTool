3225eb3bcfc26de60bbdceb71932c3a35de073e6
PolyTool <45755076+PolyTool@users.noreply.github.com>
2019-01-14 12:05:33 +0100

    Create Test du 14 Janvier.ino

---
+++ Codes Arduino/Test du 14 Janvier.ino
@@ -0,0 +1,62 @@
+// Code Arduino pour le fonctionnement du Pulse sensor avec l'ajout du Bluetooth
+
+#define USE_ARDUINO_INTERRUPTS true    
+#include <PulseSensorPlayground.h>
+#include <SoftwareSerial.h>     
+
+#define RX 11
+#define TX 12
+SoftwareSerial BlueT(RX,TX);
+
+PulseSensorPlayground pulseSensor;  
+
+//  Variables
+const int pulse_sensor = A1;              
+int Threshold = 225; //Seuil pour déterminer à partir de combien on compte qu'il s'agit d'un battement
+String prefixe_bpm = "*B";
+
+const int capteur_photo = A0;
+String prefixe_son = "*S";
+String prefixe_texte = "*L";
+long int vieuxTemps = millis();
+String c0 = "Endroit eclaire";
+String c1 = "Endroit peu eclaire : pensez a allumer une lampe si cela continue";
+String c2 = "Endroit sombre : veuillez allumer une lampe pour votre securite";       
+
+void setup() {   
+
+  Serial.begin(9600);
+  BlueT.begin(9600);          
+  pulseSensor.analogInput(pulse_sensor);         
+  pulseSensor.setThreshold(Threshold);   
+  if (pulseSensor.begin()) {
+    BlueT.print(prefixe_bpm + "'Pulse sensor' pret a l'emploi !");
+  }
+  delay(100);
+}
+
+void loop() {
+  int BPM = pulseSensor.getBeatsPerMinute();  
+  if (pulseSensor.sawStartOfBeat()) {                                    
+    BlueT.print(prefixe_bpm + "BPM : " + String(BPM));                        
+  }
+
+  int luminosite = analogRead(capteur_photo);
+  Serial.println(luminosite);
+  if (luminosite<120) {
+    Serial.println(c0);
+    BlueT.print(prefixe_texte + c0); // Ecriture sur l'appareil connecté en Bluetooth 
+  }
+  if (luminosite>120 && luminosite<=350) {
+    Serial.println(c1);
+    BlueT.print(prefixe_texte + c1); // Ecriture sur l'appareil connecté en Bluetooth
+  }
+  if (luminosite>350) {
+    Serial.println(c2);
+    BlueT.print(prefixe_texte + c2); // Ecriture sur l'appareil connecté en Bluetooth
+    if (millis() - vieuxTemps > 15000) { // Lorsqu'on est trop longtemps dans la condition précédente (au delà  de 15s), on alerte l'utilisateur par le biais d'un buzzer
+      BlueT.print("*S");
+    }
+  }
+  delay(100);                    
+}
