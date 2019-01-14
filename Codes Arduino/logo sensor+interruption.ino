4026edf78635b9b45ab0bf47d3e8430100a3f16d
PolyTool <45755076+PolyTool@users.noreply.github.com>
2019-01-14 09:59:16 +0100

    Update logo sensor+interruption.ino

--- Codes Arduino/logo sensor+interruption.ino
+++ Codes Arduino/logo sensor+interruption.ino
@@ -1,6 +1,6 @@
 //Code pour le module utilisant l'effet Hall
 
-int sensor = 2;  // le input du sensor est branché sur le pin 2
+int sensor = 2;  // le input du sensor est branché su le pin 7
 int aimant = HIGH;  // HIGH signifie qu'il n'y a pas d'aimant
 volatile int tour;
 const float rayon = 0.0004;
@@ -17,27 +17,21 @@ int c;
 void setup() {
   pinMode(sensor, INPUT);
   Serial.begin(9600); 
-  attachInterrupt(0,compteur,CHANGE);
+  attachInterrupt(0,compteur,RISING);
   //temps=millis();
 }
 
 void loop() {
   aimant=analogRead(sensor);
-  if (aimant<=515){
-    digitalWrite(aimant,LOW);
-  }
-  else{
-    digitalWrite(aimant,HIGH);
-  }
   if (millis()>(temps+delai)) {
     temps=temps+delai;
     c=c+1;
-    //vitesse=((tour*2*pi)*0,032)*3,6;
+    vitesse=((tour*2*pi)*0,032)*3,6;
     v=v+vitesse;
     vm=v/c;
     distance=(2*pi)*rayon*tour;
     //Serial.print("vitesse instantanée=");
-    //Serial.print(vitesse);
+    Serial.print(vitesse);
     //Serial.println("km/h");
     //Serial.print("vitesse moyenne");
     //Serial.print(vm);
@@ -53,7 +47,5 @@ void loop() {
 void compteur(){
   tour=tour+1;
   Serial.println(tour);
-  Serial.println("Aimant détecté");
-  vitesse=((tour*2*pi)*0,032)*3,6;
-  Serial.println(vitesse);
+  
 }
