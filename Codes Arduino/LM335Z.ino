481be5ce688a08f647fb3bf343988c3316bf73e2
PolyTool <45755076+PolyTool@users.noreply.github.com>
2019-02-11 11:47:39 +0100

    Create LM335Z.ino

---
+++ Codes Arduino/LM335Z.ino
@@ -0,0 +1,20 @@
+// Code pour le capteur de température LM335Z
+
+int capteurPin =  A0;    // Port analogique pour lire la température
+
+void setup() {
+  Serial.begin(9600);
+  }
+
+void loop() { 
+  int tensionAnalogique = analogRead(capteurPin); // Lecture de la tension 
+  float tension = (tensionAnalogique * 5.0) / 1024.0; //Conversion en volt
+  float temperature = ((tensionAnalogique*5.0)/102.4)-273.15;
+
+  Serial.print(tension);
+  Serial.println(" V");
+  Serial.print(temperature);
+  Serial.println(" °C");
+
+  delay(1000);
+  }
